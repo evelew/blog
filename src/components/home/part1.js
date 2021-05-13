1966165c8693c80d269ef942019a5b04335d5390
Evellyn Lima <evellynlrl@hotmail.com>
2021-05-13 19:59:17 -0300

    Isolate home blocks inside components

---
+++ src/components/home/part1.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+const Part1 = () => {
+  return (
+    <div
+      style={{
+        alignItems: "center",
+        backgroundColor: "black",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        minHeight: "100vh",
+        minWidth: "100vw",
+      }}
+    >
+      <h1
+        style={{
+          margin: 0,
+          color: "white",
+          textAlign: "center",
+          fontSize: "96px",
+        }}
+      >
+        Evellyn Lima
+      </h1>
+      <h2
+        style={{
+          margin: "50px 0 0 0",
+          color: "white",
+          textAlign: "center",
+          fontSize: "36px",
+        }}
+      >
+        Frontend Developer
+      </h2>
+    </div>
+  )
+}
+
+export default Part1
---
+++ src/components/home/part2.js
@@ -0,0 +1,56 @@
+import React from "react"
+
+const Part2 = () => {
+  return (
+    <div
+      style={{
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        minHeight: "100vh",
+        minWidth: "100vw",
+      }}
+    >
+      <h2
+        style={{
+          margin: "50px 0 0 0",
+          color: "#FFB347",
+          textAlign: "center",
+          fontSize: "36px",
+        }}
+      >
+        Quem é ela
+      </h2>
+
+      <div
+        style={{
+          fontSize: "18px",
+          textAlign: "center",
+          margin: "60px 0 0 0",
+          width: "1200px",
+        }}
+      >
+        <p>
+          Eu sou a Evellyn, tenho 25 anos, sou de Curitiba mas agora tô morando
+          em São Paulo. Gosto muito de ouvir música em qualquer momento do dia,
+          assim que acordo, no banho, lavando louça, varrendo a casa, estudando
+          e etc. Gosto de quase todo tipo de música, mas o que mais ouço é funk
+          e pop (principalmente brasileiro). Gosto muito de conversar, então
+          meus roles favoritos envolvem mesa + cerveja + conversa. (acaba logo
+          quarentena!!!)
+        </p>
+        <p>
+          E sobre a parte profissional, eu trabalho com frontend desde 2013, já
+          passei por algumas empresas com modelos e stacks diferentes e essas
+          experiências hoje me ajudam a pensar em soluções/maneiras diferentes
+          de resolver problemas. O que mais gosto de fazer é mexer com JS e
+          refatorar/otimizar soluções. No próximo bloco você consegue ver com
+          mais detalhes quais tecnologias eu tenho experiência.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Part2
