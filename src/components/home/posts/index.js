2435a416d74e8ec8ec16bc933d24d3681186ef1c
Evellyn Lima <evellynlrl@hotmail.com>
2021-05-13 23:20:58 -0300

    Get last posts from blog

--- src/components/home/posts/index.js
+++ src/components/home/posts/index.js
@@ -2,35 +2,21 @@ import React from "react"
 
 import "./styles.scss"
 
-const Posts = () => {
+const Posts = ({ posts }) => {
   return (
     <div className="posts">
       <h2>Últimos posts</h2>
 
       <div className="posts__list">
-        <a href="#" className="posts__card">
-          <img
-            src="https://www.blendit.com/wp-content/uploads/2018/08/low-code-o-que-e.jpg"
-            alt=""
-          />
-          <h3>como testar o teste</h3>
-        </a>
-
-        <a href="#" className="posts__card">
-          <img
-            src="https://www.blendit.com/wp-content/uploads/2018/08/low-code-o-que-e.jpg"
-            alt=""
-          />
-          <h3>como testar o teste</h3>
-        </a>
-
-        <a href="#" className="posts__card">
-          <img
-            src="https://www.blendit.com/wp-content/uploads/2018/08/low-code-o-que-e.jpg"
-            alt=""
-          />
-          <h3>como testar o teste</h3>
-        </a>
+        {posts.map(({ node: { fields: { slug }, frontmatter: { title } } }) => (
+          <a href={slug} className="posts__card" key={slug}>
+            <img
+              src="https://www.blendit.com/wp-content/uploads/2018/08/low-code-o-que-e.jpg"
+              alt=""
+            />
+            <h3>{title}</h3>
+          </a>
+        ))}
       </div>
     </div>
   )
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import SEO from "../components/seo"
 import TopBlock from "../components/home/topBlock"
@@ -6,26 +7,44 @@ import About from "../components/home/about"
 import Knowledge from "../components/home/knowledge"
 import Posts from "../components/home/posts"
 
-class BlogIndex extends React.Component {
-  render() {
-    return (
-      <>
-        <SEO title="Home" />
+const BlogIndex = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges
+  return (
+    <>
+      <SEO title="Home" />
 
-        <section
-          style={{
-            minHeight: "100vh",
-            minWidth: "100vw",
-          }}
-        >
-          <TopBlock />
-          <About />
-          <Knowledge />
-          <Posts />
-        </section>
-      </>
-    )
-  }
+      <section
+        style={{
+          minHeight: "100vh",
+        }}
+      >
+        <TopBlock />
+        <About />
+        <Knowledge />
+        <Posts posts={posts} />
+      </section>
+    </>
+  )
 }
 
 export default BlogIndex
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 3
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+`
