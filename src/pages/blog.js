f4919a7655eb922df6059b41e0b82d05726aacbe
Evellyn Lima <evellynlrl@hotmail.com>
2021-06-28 17:32:42 -0300

    Remove siteTitle from the blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -13,7 +13,7 @@ class BlogIndex extends React.Component {
     const posts = data.allMarkdownRemark.edges
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <Layout location={this.props.location}>
         <SEO title="Home" />
         <Bio />
         {posts.map(({ node }) => {
