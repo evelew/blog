c53c6602babd5d9d839d616c6fcee3bf80c61e3e
Evellyn Lima <evellynlrl@hotmail.com>
2021-12-14 21:01:46 -0300

    Add dynamic data inside the post page

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,29 +1,77 @@
+import { useRouter } from "next/router";
+
+import markdownToHtml from "../helpers/remark";
+import { getAllPosts, getPostBySlug } from "../helpers/api";
+
+import ArrowLeft from "../../public/images/arrow-left.svg";
+
 import s from "../../styles/BlogPost.module.scss";
 
-const Post = () => {
+const Post = ({ post }) => {
+  const router = useRouter();
+
   return (
     <>
       <div className={s.hero}>
-        <div className={s["hero-image"]} />
+        <div className={s["hero-wrapper"]}>
+          <button className={s["hero-button"]} onClick={() => router.back()}>
+            <ArrowLeft />
+          </button>
+        </div>
+        <div
+          className={s["hero-image"]}
+          style={{ backgroundImage: `url(${post.cover_image})` }}
+        />
       </div>
       <main className={s.body}>
-        <small className={s.date}>September 27, 2021</small>
-        <h1 className={s.title}>Criando atalhos para os comandos do git</h1>
-        <h2 className={s.subtitle}>
-          A vida é curta demais pra ficar digitando comandos longos ⌨️
-        </h2>
-
-        <div className={s.text}>
-          <p>
-            Tem muitos comandos do git que são difíceis de lembrar de cabeça e
-            pra não precisar ficar buscando toda vez como fazer aquele comando
-            que você quase nunca usa você pode criar atalhos no git, e o melhor
-            é que essa criação é super simples.
-          </p>
-        </div>
+        <small className={s.date}>{post.date}</small>
+        <h1 className={s.title}>{post.title}</h1>
+        <h2 className={s.subtitle}>{post.description}</h2>
+        <div
+          className={s.text}
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        ></div>
       </main>
     </>
   );
 };
 
 export default Post;
+
+export async function getStaticProps({ params }) {
+  const post = getPostBySlug(params.slug, [
+    "title",
+    "description",
+    "date",
+    "cover_image",
+    "credit_image",
+    "credit_image_author",
+    "content",
+  ]);
+
+  const content = await markdownToHtml(post.content || "");
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(["slug"]);
+
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
